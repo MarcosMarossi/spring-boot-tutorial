97d56714e1657b7ac483351bcd2b7b58895e3996
Marcos Marossi <marcos_marossi@hst.com.br>
2020-03-16 13:29:31 -0300

    Add Order, relationships between table User and Order

--- course/src/main/java/com/course/model/User.java
+++ course/src/main/java/com/course/model/User.java
@@ -1,13 +1,18 @@
 package com.course.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{	
 	private static final long serialVersionUID = 1L;
 	
@@ -19,15 +24,20 @@ public class User implements Serializable{
 	private String email;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();	
+	
 	public User () {} 
 
 	public User(String name, String phone, String email, String password) {
-		super();
-		this.id = id;
 		this.name = name;
 		this.phone = phone;
 		this.email = email;
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	public Integer getId() {
