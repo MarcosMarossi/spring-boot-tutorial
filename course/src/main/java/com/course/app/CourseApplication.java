2af2a9deca974844e380a6975fc7e436cbe9fc3e
Marcos Marossi <marcos_marossi@hst.com.br>
2020-03-16 09:55:57 -0300

    Add User and UserResource

--- course/src/main/java/com/course/app/CourseApplication.java
+++ course/src/main/java/com/course/app/CourseApplication.java
@@ -1,4 +1,4 @@
-package com.course.udemy;
+package com.course.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ course/src/main/java/com/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.course.model.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping	
+	public ResponseEntity<User> findAll (){
+		User u = new User(1, "HST", "(19)9988556677", "hst@hst.com.br", "66");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
