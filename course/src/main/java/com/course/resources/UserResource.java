488d64a5bae5f16cfef0946c7afa515d49b54b4c
Marcos Marossi <marcos_marossi@hst.com.br>
2020-03-16 10:27:55 -0300

    H2 Database and JPA

--- course/src/main/java/com/course/resources/UserResource.java
+++ course/src/main/java/com/course/resources/UserResource.java
@@ -11,7 +11,7 @@ import com.course.model.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 	
-	@GetMapping	
+	@GetMapping
 	public ResponseEntity<User> findAll (){
 		User u = new User(1, "HST", "(19)9988556677", "hst@hst.com.br", "66");
 		return ResponseEntity.ok().body(u);
