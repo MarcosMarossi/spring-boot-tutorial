de68d644c2b2b2b0d7ded83a2107eb294e3bbbf7
Marcos Marossi <marcos.marossi@fatec.sp.gov.br>
2020-03-19 14:31:02 -0300

    Insert new users

--- course/src/main/java/com/course/services/UserService.java
+++ course/src/main/java/com/course/services/UserService.java
@@ -23,5 +23,9 @@ public class UserService {
 		Optional<User> user = userRespository.findById(id);
 		return user.get();
 	}
+	
+	public User insert(User user) {
+		return userRespository.save(user);
+	}
 
 }
