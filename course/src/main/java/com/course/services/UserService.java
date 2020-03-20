c227a378511ce3098d1ed031316f74922c75afe5
Marcos Marossi <marcos.marossi@fatec.sp.gov.br>
2020-03-20 09:13:32 -0300

    Exception handling update

--- course/src/main/java/com/course/services/UserService.java
+++ course/src/main/java/com/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -15,42 +17,45 @@ import com.course.services.exception.ResourceNotFountException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	UserRepository userRespository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRespository.findAll();
 	}
-	
-	public User findById(Integer id){
+
+	public User findById(Integer id) {
 		Optional<User> user = userRespository.findById(id);
-		return user.orElseThrow(
-					() -> new ResourceNotFountException(id)
-				);
+		return user.orElseThrow(() -> new ResourceNotFountException(id));
 	}
-	
+
 	public User insert(User user) {
 		return userRespository.save(user);
 	}
-	
+
 	public void delete(Integer id) {
 		try {
 			userRespository.deleteById(id);
-			
+
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFountException(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
-			
+
 	}
-	
-	public User update (Integer id, User user) {
-		User entity = userRespository.getOne(id);	
-		updateData(entity, user);
-		return userRespository.save(entity);	
-}
+
+	public User update(Integer id, User user) {
+		try {
+			User entity = userRespository.getOne(id);
+			updateData(entity, user);
+			return userRespository.save(entity);
+
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFountException(id);
+		}
+	}
 
 	private void updateData(User entity, User user) {
 		entity.setName(user.getName());
