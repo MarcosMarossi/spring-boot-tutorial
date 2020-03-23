5fde51febf9092625e2b47c064f797343664c6ff
Marcos Marossi <marcos.marossi@fatec.sp.gov.br>
2020-03-23 15:09:08 -0300

    Reference posts of user

--- workshopmongo/src/main/java/com/marcos/workshop/config/Test.java
+++ workshopmongo/src/main/java/com/marcos/workshop/config/Test.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.marcos.workshop.dto.AuthorDTO;
 import com.marcos.workshop.model.Post;
 import com.marcos.workshop.model.User;
 import com.marcos.workshop.repository.PostRespository;
@@ -35,14 +36,16 @@ public class Test implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 =  new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 =  new Post(null, sdf.parse("19/03/2020"), "Vai viajar?", "Queria saber isso, pois combinamos de irmos juntos", alex);
-		Post post3 =  new Post(null, sdf.parse("22/03/2020"), "Bom dia", "Acordei feliz hoje!", bob	);
-		
-		
-		post.saveAll(Arrays.asList(post1,post2,post3));
 		user.saveAll(Arrays.asList(maria, alex, bob));
+				
+		Post post1 =  new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 =  new Post(null, sdf.parse("19/03/2020"), "Vai viajar?", "Queria saber isso, pois combinamos de irmos juntos", new AuthorDTO(alex));
+		Post post3 =  new Post(null, sdf.parse("22/03/2020"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(bob));
+				
+		post.saveAll(Arrays.asList(post1,post2,post3));
 		
 		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		user.save(maria);
 	}
 }
---
+++ workshopmongo/src/main/java/com/marcos/workshop/dto/AuthorDTO.java
@@ -0,0 +1,37 @@
+package com.marcos.workshop.dto;
+
+import java.io.Serializable;
+
+import com.marcos.workshop.model.User;
+
+public class AuthorDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+
+	public AuthorDTO() {
+
+	}
+
+	public AuthorDTO(User user) {
+		id = user.getId();
+		name = user.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
