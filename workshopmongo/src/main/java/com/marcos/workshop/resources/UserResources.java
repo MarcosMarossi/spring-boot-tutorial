55ae6b72a91c68b6b07f8ab51782123f64d68ba0
Marcos Marossi <marcos.marossi@fatec.sp.gov.br>
2020-03-23 11:50:32 -0300

    Add UserDTO

--- workshopmongo/src/main/java/com/marcos/workshop/resources/UserResources.java
+++ workshopmongo/src/main/java/com/marcos/workshop/resources/UserResources.java
@@ -3,12 +3,15 @@ package com.marcos.workshop.resources;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.marcos.workshop.dto.UserDTO;
 import com.marcos.workshop.model.User;
 import com.marcos.workshop.services.UserService;
 
@@ -20,9 +23,10 @@ public class UserResources {
 	private UserService user;
 	
 	@GetMapping
-	public List<User> findAll() {		
+	public ResponseEntity<List<UserDTO>>findAll() {		
 		List<User> list = user.findAll();
-		return list;		
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);		
 	}
 
 }
