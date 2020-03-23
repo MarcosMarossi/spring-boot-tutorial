e23593e02279c3af3bdfcbad7a3d8ff907b7ce7c
Marcos Marossi <marcos.marossi@fatec.sp.gov.br>
2020-03-23 13:31:16 -0300

    Insert new users

--- workshopmongo/src/main/java/com/marcos/workshop/resources/UserResources.java
+++ workshopmongo/src/main/java/com/marcos/workshop/resources/UserResources.java
@@ -1,5 +1,6 @@
 package com.marcos.workshop.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marcos.workshop.dto.UserDTO;
 import com.marcos.workshop.model.User;
@@ -34,6 +38,14 @@ public class UserResources {
 	public ResponseEntity<UserDTO> findById( @PathVariable String id) {		
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert( @RequestBody UserDTO userDto ) {		
+		User user = service.fromDTO(userDto);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- workshopmongo/src/main/java/com/marcos/workshop/services/UserService.java
+++ workshopmongo/src/main/java/com/marcos/workshop/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.marcos.workshop.dto.UserDTO;
 import com.marcos.workshop.model.User;
 import com.marcos.workshop.repository.UserRespository;
 import com.marcos.workshop.services.exception.ObjectNotFoundException;
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repoUser.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found. Id " + id));
+	}
+	
+	public User insert(User user) {
+		return repoUser.insert(user);
+	}
+	
+	public User fromDTO (UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 
 }
