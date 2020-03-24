5aa17564decc87ffc96526fe1497bf44996f721f
Marcos Marossi <marcos.marossi@fatec.sp.gov.br>
2020-03-24 09:22:37 -0300

    Query with regex

--- workshopmongo/src/main/java/com/marcos/workshop/repository/PostRespository.java
+++ workshopmongo/src/main/java/com/marcos/workshop/repository/PostRespository.java
@@ -3,6 +3,7 @@ package com.marcos.workshop.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.marcos.workshop.model.Post;
@@ -11,6 +12,9 @@ import com.marcos.workshop.model.User;
 @Repository
 
 public interface PostRespository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchText (String text);
 	
 	List<Post> findByTitleContainingIgnoreCase (String text);
 	
